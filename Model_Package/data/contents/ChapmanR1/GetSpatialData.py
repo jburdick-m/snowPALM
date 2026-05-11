5577ea3d9921f584bd7e556ba4e0953e4f6fcf74
jburdick-m <jburdick.m@gmail.com>
2026-05-11 15:25:28 -0700

    update veght filepaths

--- Model_Package/data/contents/ChapmanR1/GetSpatialData.py
+++ Model_Package/data/contents/ChapmanR1/GetSpatialData.py
@@ -37,9 +37,9 @@ pars['IndexDir'] = os.getcwd() + '\\Preprocess\\'  # GIS Directory
 # Elevation raster to get data from
 pars['DTM_File'] = r'InputData\SpatialData\chapman_DEM.tif'
 # Canopy height raster to get data from
-pars['VegHT_File'] = r'InputData\SpatialData\chapman_CHM.tif'
+pars['VegHT_File'] = r'InputData\SpatialData\chapman_CHM_1point5m.tif'
 # Canopy cover raster to get data from
-pars['VegCover_File'] = r'InputData\SpatialData\chapman_CC.tif'
+pars['VegCover_File'] = r'InputData\SpatialData\chapman_CC_1point5m.tif'
 # Cutline file to clip out shape (will only be used if pars['Cutline_File'] is not an empty string)
 pars['Cutline_File'] = ''
 
@@ -48,7 +48,7 @@ pars['UseOriginalPixels'] = False                    # Force model boundaries to
 
 # The following parameters are only used if UseOriginalPixels is set to False
 pars['Target_SRS'] = 'EPSG:26910'                       # Spatial Reference System (Proj4)
-pars['CellSize'] = 0.5                                    # Model Cell Size
+pars['CellSize'] = 1.5                                    # Model Cell Size
 pars['CellSize_LowRes'] = 30                            # Low resolution cell size (for interpolation of forcing data)
 pars['Resample'] = 'average'                            # Resampling Method (near(default) bilinear, cubic, cubicspline, lanczos, 
                                                         # average, rms, mode, max, min, med, Q1, Q3, sum)
